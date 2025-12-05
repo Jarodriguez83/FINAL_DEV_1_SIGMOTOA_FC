57cd0eae560025e87346601049453f0b06153745
Jader Santiago Nieves Tami <jassan0612@gmail.com>
2025-12-05 11:59:33 -0500

    Modi

--- main.py
+++ main.py
@@ -44,13 +44,30 @@ SUPABASE_URL: str = "https://okuotijfayaoecerimfi.supabase.co"
 SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rdW90aWpmYXlhb2VjZXJpbWZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3OTg1OTMsImV4cCI6MjA4MDM3NDU5M30.8SstgKcCZs3CbcZSd0KEH4FQ7VBEnLR3t5RJeBzvsxk"
 SUPABASE_BUCKET_NAME = "IMG"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY) #CREACIÓN DEL CLIENTE DE SUPABASE
-#EJECUCIÓN DE LA CREACIÓN DE LA BASE DE DATOS Y TABLAS AL INICIAR EL SISTEMA WEB 
+
+#EJECUCIÓN DE LA CREACIÓN DE LA BASE DE DATOS Y TABLAS AL INICIAR EL SISTEMA WEB
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
-#RUTAS BÁSICAS
+
+# RUTA BÁSICA: INICIO
 @app.get("/", tags=["FRONTEND"])
 def homepage(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "title": "SIGMOTOA FC - SISTEMA WEB DE GESTIÓN DE DATOS"})
+
+# RUTA HTML: LISTADO DE JUGADORES
+@app.get("/jugadores", response_class=HTMLResponse, tags=["FRONTEND"])
+def listar_jugadores(request: Request, session: Session = Depends(get_session)):
+    jugadores = session.exec(select(Jugador)).all()
+    return templates.TemplateResponse(
+        "jugadores.html",
+        {
+            "request": request,
+            "jugadores": jugadores,
+            "title": "Listado de Jugadores"
+        }
+    )
+
+
 
 
